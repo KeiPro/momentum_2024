70e9c43721444986842d2726dcfdabec69361a2c
keipro <keiprogramo@gmail.com>
2024-01-16 16:18:10 +0900

    main goal edit기능 추가 및 새로고침 시 체크박스 처리 안되는 버그 수정.

--- js/maingoal.js
+++ js/maingoal.js
@@ -1,14 +1,17 @@
 import { USER_NAME_KEY } from "./greetings.js";
 
 const MAIN_GOAL_KEY = "main_goal";
+const COMPLETE_GOAL_KEY = "main_goal_complete";
 
 const mainGoalContainer = document.querySelector(".main-goal-container");
 const mainGoalForm = document.getElementById("main-goal-form");
 const mainGoalInput = mainGoalForm.querySelector("input");
 const mainGoalTitle = document.querySelector(".main-goal-title");
 const mainGoalText = document.querySelector(".main-goal");
+
 const mainGoalItem = mainGoalContainer.querySelector(".main-goal-item");
 const mainGoalCheckBox = mainGoalItem.querySelector(".check-box");
+const mainGoalEditButton = mainGoalItem.querySelector(".main-goal-edit");
 
 const HIDDEN_CLASSNAME = "hidden";
 
@@ -24,12 +27,18 @@ function handleMainGoalSubmit(event)
     //main-goal 변경.
     const newMainGoal = mainGoalInput.value;
     mainGoalText.innerText = newMainGoal;
+    prevMainGoal = newMainGoal;
     
     //main-goal 데이터 저장.
     localStorage.setItem(MAIN_GOAL_KEY, newMainGoal);
+    localStorage.setItem(COMPLETE_GOAL_KEY, 'false');
+
+    refreshMainGoalCheckBox();
 }
 
-function switchMainGoalState(hasMainGoal)
+let prevMainGoal = "";
+
+function toggleMainGoalDisplay(hasMainGoal)
 {
     if(hasMainGoal)
     {
@@ -40,6 +49,8 @@ function switchMainGoalState(hasMainGoal)
     else
     {
         mainGoalForm.classList.remove(HIDDEN_CLASSNAME);
+        mainGoalInput.value = prevMainGoal;
+
         mainGoalText.classList.add(HIDDEN_CLASSNAME);
         mainGoalItem.classList.add(HIDDEN_CLASSNAME);
     }
@@ -57,41 +68,71 @@ function showMainGoalTitle(hasMainGoal)
     }
 }
 
-//---------------------------------------------------------------------------------//
-
-const savedUserName = localStorage.getItem(USER_NAME_KEY);
-if(savedUserName === null)
-{
-    mainGoalContainer.classList.add(HIDDEN_CLASSNAME);
-
-    document.addEventListener('loginSuccess', function() {
-        mainGoalContainer.classList.remove(HIDDEN_CLASSNAME);
-        
-        showMainGoalTitle(false);
-        switchMainGoalState(false);
-    });
-}
-
-const savedMainGoal = localStorage.getItem(MAIN_GOAL_KEY);
-if(savedMainGoal !== null)
-{
-    showMainGoalTitle(true);
-    switchMainGoalState(true);
-
-    mainGoalText.innerText = savedMainGoal;
-}
-else
+function editMainGoal()
 {
     showMainGoalTitle(false);
+    toggleMainGoalDisplay(false);
+
+    mainGoalInput.focus();
 
     mainGoalForm.addEventListener("submit", handleMainGoalSubmit);
 }
 
-mainGoalCheckBox.addEventListener('change', function(){
-    const todoText = this.nextElementSibling;
+function refreshMainGoalCheckBox()
+{
+    const isChecked = localStorage.getItem(COMPLETE_GOAL_KEY) === 'true';
+    mainGoalCheckBox.checked = isChecked;
+    mainGoalCheckBox.nextElementSibling.style.textDecoration = isChecked ? 'line-through' : 'none';
+}
+
+function changeCheckBoxState()
+{
+    const todoText = mainGoalCheckBox.nextElementSibling;
     if(this.checked) {
         todoText.style.textDecoration = 'line-through';
+        localStorage.setItem(COMPLETE_GOAL_KEY, 'true');
     }else {
         todoText.style.textDecoration = 'none';
+        localStorage.setItem(COMPLETE_GOAL_KEY, 'false');
     }
-})
+}
+
+function loginSuccess()
+{
+    mainGoalContainer.classList.remove(HIDDEN_CLASSNAME);
+    editMainGoal();
+
+    mainGoalCheckBox.addEventListener('change', changeCheckBoxState);
+    mainGoalEditButton.addEventListener('click', editMainGoal);
+}
+
+//---------------------------------------------------------------------------------//
+
+const savedUserName = localStorage.getItem(USER_NAME_KEY);
+if(savedUserName !== null)
+{
+    const savedMainGoal = localStorage.getItem(MAIN_GOAL_KEY);
+    if(savedMainGoal !== null)
+    {
+        showMainGoalTitle(true);
+        toggleMainGoalDisplay(true);
+
+        mainGoalText.innerText = savedMainGoal;
+        prevMainGoal = savedMainGoal;
+
+        refreshMainGoalCheckBox();
+    }
+    else
+    {
+        editMainGoal();
+    }
+
+    mainGoalCheckBox.addEventListener('change', changeCheckBoxState)
+    mainGoalEditButton.addEventListener('click', editMainGoal);
+}
+else
+{
+    mainGoalContainer.classList.add(HIDDEN_CLASSNAME);
+    document.addEventListener('loginSuccess', loginSuccess());
+}
+
