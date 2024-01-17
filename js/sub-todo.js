e00ce6f1d47c318c961efbab523f625de9eed55e
keipro <keiprogramo@gmail.com>
2024-01-17 19:03:10 +0900

    todolist container transition추가(위치 이동 및 크기 변화)

--- js/sub-todo.js
+++ js/sub-todo.js
@@ -2,22 +2,32 @@ const todoButton = document.querySelector(".sub-to-do-list > span")
 const todoListContainer = document.querySelector('.to-do-list__container');
 
 const HIDDEN_CLASSNAME = "hidden";
+let isToggleChanging = false;
 
 function toggleToDoList()
 {
+    if(isToggleChanging)
+        return;
+
+    isToggleChanging = true;
+
     if(todoListContainer.classList.contains(HIDDEN_CLASSNAME))
     {
         todoListContainer.classList.remove(HIDDEN_CLASSNAME);
         setTimeout(() => {
             todoListContainer.style.opacity = '1';
+            todoListContainer.style.transform = 'translateY(0) scale(1)';
+            isToggleChanging = false;
         }, 10); // display: none을 해제한 후 약간의 지연을 줍니다.
     }
     else
     {
         todoListContainer.style.opacity = '0';
+        todoListContainer.style.transform = 'translateY(10px) scale(0.95)';
         setTimeout(() => {
             todoListContainer.classList.add(HIDDEN_CLASSNAME);
-        }, 200); // 500ms는 transition 시간과 일치해야 합니다.
+            isToggleChanging = false;
+        }, 100); // 500ms는 transition 시간과 일치해야 합니다.
     }
 }
 
